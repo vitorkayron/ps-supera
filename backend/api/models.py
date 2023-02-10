cec0f1af76f0ec2c864cf7e1ec2ca06197d016f3
Vitor Kayron <vitork2016@gmail.com>
2023-02-10 12:56:21 -0300

    Add new model and serializer for Products

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30, blank=True, null=False)
+    price = models.DecimalField(blank=True)
+    score = models.IntegerField(blank=True)
